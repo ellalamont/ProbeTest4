a7e225367d8f5a80a4782073eddf394e05bdbb73
ellalamont <elamont@uw.edu>
2025-01-15 14:59:14 -0800

    Started script to make graph for limit of detection samples

---
+++ THP1_LimitofDetection.R
@@ -0,0 +1,29 @@
+# Limit of Detection with the THP1 spiked samples graphs
+# E. Lamont
+# 1/15/25
+
+source("Import_data.R") # to get my_pipeSummary
+
+# Only looking at the not pooled samples because it didn't work well when I pooled the high and low concentration samples together
+
+# Plot basics
+my_plot_themes <- theme_bw() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  theme(legend.position = "right",legend.text=element_text(size=14),
+        legend.title = element_text(size = 14),
+        plot.title = element_text(size=10), 
+        axis.title.x = element_text(size=14), 
+        axis.text.x = element_text(angle = 0, size=14, vjust=0, hjust=0.5),
+        axis.title.y = element_text(size=14),
+        axis.text.y = element_text(size=14), 
+        plot.subtitle = element_text(size=9), 
+        plot.margin = margin(10, 10, 10, 20),
+        panel.background = element_rect(fill='transparent'),
+        plot.background = element_rect(fill='transparent', color=NA),
+        legend.background = element_rect(fill='transparent'),
+        legend.box.background = element_blank()
+  )
+
+
+###########################################################
+############ SCATTER: CELL NUMBER VS N_GENOMIC ############
