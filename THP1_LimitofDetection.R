7752e403c099b57b26ebb93afeda192ebfea014d
ellalamont <elamont@uw.edu>
2025-01-16 14:54:40 -0800

    Added to Limit of Detection script

--- THP1_LimitofDetection.R
+++ THP1_LimitofDetection.R
@@ -24,6 +24,51 @@ my_plot_themes <- theme_bw() +
         legend.box.background = element_blank()
   )
 
+###########################################################
+############## FILTER JUST THE SAMPLES I WANT #############
+
+LimitofDetect <- my_pipeSummary %>% filter(grepl("new", SampleID)) %>% filter(Pooled_Set == "No") %>% mutate(Ra_cells2 = c(1e2, 1e3, 1e4, 1e5, 1e6))
+
 
 ###########################################################
 ############ SCATTER: CELL NUMBER VS N_GENOMIC ############
+
+# N_GENOMIC
+LimitofDetect_NumReads_Fig1 <- LimitofDetect %>% 
+  ggplot(aes(x = Ra_cells2, y = N_Genomic)) + 
+  geom_point(size = 6, alpha = 0.8, stroke = 0.8, fill = "#03A9F4", shape = 21) + 
+  geom_text_repel(aes(label = format(N_Genomic, big.mark = ",")), size= 3, box.padding = 0.4, segment.color = NA) + 
+  geom_hline(yintercept = 1000000, linetype = "dashed", alpha = 0.5) + 
+  labs(title = "THP1 cells spiked with H37Ra",
+       subtitle = "Label is number of reads aligned to Mtb", 
+       x = "# spiked in H37Ra cells", 
+       y = "# reads aligning to Mtb genome") + 
+  scale_x_continuous(trans = "log10") + 
+  my_plot_themes
+LimitofDetect_NumReads_Fig1
+ggsave(LimitofDetect_NumReads_Fig1,
+       file = "LimitofDetect_NumReads_1.pdf",
+       path = "LimitofDetection",
+       width = 6, height = 4, units = "in")
+
+# P_GENOMIC
+LimitofDetect_PercentReads_Fig1 <- LimitofDetect %>% 
+  ggplot(aes(x = Ra_cells2, y = P_Genomic)) + 
+  geom_point(size = 6, alpha = 0.8, stroke = 0.8, fill = "#03A9F4", shape = 21) + 
+  geom_text_repel(aes(label = format(N_Genomic, big.mark = ",")), size= 3, box.padding = 0.4, segment.color = NA) + 
+  labs(title = "THP1 cells spiked with H37Ra",
+       subtitle = "Label is number of reads aligned to Mtb", 
+       x = "# spiked in H37Ra cells", 
+       y = "% reads aligning to Mtb genome") + 
+  scale_x_continuous(trans = "log10") + 
+  my_plot_themes
+LimitofDetect_PercentReads_Fig1
+ggsave(LimitofDetect_PercentReads_Fig1,
+       file = "LimitofDetect_PercentReads_1.pdf",
+       path = "LimitofDetection",
+       width = 6, height = 4, units = "in")
+
+
+
+
+
